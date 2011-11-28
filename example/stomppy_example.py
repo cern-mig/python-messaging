6f0beaf8cac09b3a728938dcd56a1f2f6e1089f9
Massimo <massimo.paladin@gmail.com>
2011-11-28 18:01:27 +0100

    adding stomppy support

---
+++ example/stomppy_example.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+from messaging.message import Message
+from messaging.stomppy import MessageListener
+import stomp
+import time
+
+class MyListener(MessageListener):
+    def __init__(self):
+        self.uuid = None
+        self.done = False
+    
+    def error(self, message):
+        print("received an error %s" % message)
+
+    def message(self, message):
+        if message.header.get("x-uuid") == self.uuid:
+            self.done = True
+        print("received message %s" % message)
+        
+def stomppy_test():
+    """
+    Example that shows how to handle messages
+    """
+    print("stomppy example")
+    conn = stomp.Connection([("dev.msg.cern.ch", 6163)])
+    listener = MyListener()
+    conn.set_listener("", listener)
+    conn.start()
+    conn.connect()
+    
+    msg = Message(body="stomppy_test",
+                  header={'destination' : '/topic/test.stomppy',
+                          'x-uuid' : "%s" % time.time()})
+    listener.uuid = msg.header['x-uuid']
+    conn.subscribe(destination='/topic/test.stomppy', ack='auto')
+    conn.send(msg.body, **msg.header)
+    print("sending message %s" % msg)
+    
+    t1 = time.time()
+    while not listener.done and (time.time() - t1 < 2):
+        time.sleep(0.1)
+    conn.disconnect()
+    print("...stomppy example ok")
+    
+def main():
+    stomppy_test()
+
+if __name__ == "__main__":
+    main()
---
+++ messaging/stomppy.py
@@ -0,0 +1,106 @@
+"""
+This class add support for Message module in stomppy default listener.
+It can be passed directly to Connection.set_listener().
+"""
+
+from messaging.message import Message
+
+class MessageListener(object):
+    """
+    This class add support for Message module in stomppy default listener.
+    It can be passed directly to Connection.set_listener().
+    """
+    def on_connecting(self, host_and_port):
+        """
+        Called by the STOMP connection once a TCP/IP connection to the
+        STOMP server has been established or re-established. Note that
+        at this point, no connection has been established on the STOMP
+        protocol level. For this, you need to invoke the "connect"
+        method on the connection.
+
+        \param host_and_port a tuple containing the host name and port
+        number to which the connection has been established.
+        """
+        pass
+
+    def on_connected(self, headers, body):
+        """ Translate standard call to custom one. """
+        self.connected(Message(header=headers, body=body))
+        
+    def connected(self, message):
+        """
+        Called by the STOMP connection when a CONNECTED frame is
+        received, that is after a connection has been established or
+        re-established.
+
+        \param message received from the server.
+        """
+        pass
+
+    def on_disconnected(self):
+        """
+        Called by the STOMP connection when a TCP/IP connection to the
+        STOMP server has been lost.  No messages should be sent via
+        the connection until it has been reestablished.
+        """
+        pass
+        
+    def on_heartbeat_timeout(self):
+        """
+        Called by the STOMP connection when a heartbeat message has not been
+        received beyond the specified period.
+        """
+        pass
+
+    def on_message(self, headers, body):
+        """ Translate standard call to custom one. """
+        self.message(Message(header=headers, body=body))
+        
+    def message(self, message):
+        """
+        Called by the STOMP connection when a MESSAGE frame is
+        received.
+
+        \param message received from the server.
+        """
+        pass
+
+    def on_receipt(self, headers, body):
+        """ Translate standard call to custom one. """
+        self.receipt(Message(header=headers, body=body))
+        
+    def receipt(self, message):
+        """
+        Called by the STOMP connection when a RECEIPT frame is
+        received, sent by the server if requested by the client using
+        the 'receipt' header.
+
+        \param message received from the server.
+        """
+        pass
+    
+    def on_error(self, headers, body):
+        """ Translate standard call to custom one. """
+        self.error(Message(header=headers, body=body))
+
+    def error(self, message):
+        """
+        Called by the STOMP connection when an ERROR frame is
+        received.
+
+        \param message received from the server.
+        """
+        pass
+    
+    def on_send(self, headers, body):
+        """ Translate standard call to custom one. """
+        self.send(Message(header=headers, body=body))
+    
+    def send(self, message):
+        """
+        Called by the STOMP connection when it is in the process
+        of sending a message.
+        
+        \param message being sent.
+        """
+        pass
